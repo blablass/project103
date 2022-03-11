837887e2c6b3eb62d88b2aacefe9019b6c2b0afa
blablass <82516957+blablass@users.noreply.github.com>
2022-03-11 17:42:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach(camera);
+function clickPICTURE(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML  = "<img src='"+ data_uri +"' id='anyid'>"
+    });
+}
+console.log("ml5 version",ml5.version)
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/MjDp6tXjH/model.json",modelloaded);
+function modelloaded(){
+    console.log("modelloaded");
+}
+function idenimg(){
+    img = document.getElementById("anyid")
+    classifier.classify(img,getresult)
+}
+function getresult(error,result){
+    if(error){
+        console.log(error)
+    }else{
+        console.log(result)
+        document.getElementById("Object_name").innerHTML = result[0].label
+        document.getElementById("accuracy").innerHTML = result[0].confidence.toFixed(3)
+    }
+}
